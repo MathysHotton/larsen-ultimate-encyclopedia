7a69830c4211956f41ed90f947616b05a81f7a95
Mathys Hotton <phoenix2799.xbox@gmail.com>
2026-07-31 09:40:30 +0200

    Support multipage Vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
 
@@ -6,7 +7,23 @@ export default defineConfig({
 
     build: {
 
-        outDir: "docs"
+        outDir: "docs",
+
+        rollupOptions: {
+
+            input: {
+
+                main: resolve(__dirname, "index.html"),
+
+                explorer: resolve(__dirname, "src/pages/explorer.html"),
+
+                encyclopedia: resolve(__dirname, "src/pages/encyclopedia.html"),
+
+                tree: resolve(__dirname, "src/pages/tree.html")
+
+            }
+
+        }
 
     }
 
